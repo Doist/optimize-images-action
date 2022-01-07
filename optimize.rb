e6386b04101019306ba1d3c1544d3ceb891c1077
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-07 07:41:31 +0000

    Prefer Oxipng over other PNG optimizers
    Oxipng is faster, with better or comparable results. It's enabled by
    default in image_optim since v0.31.0.
    
    There are many resources online, this is a good one:
    https://css-ig.net/benchmark/png-lossless

--- optimize.rb
+++ optimize.rb
@@ -29,10 +29,10 @@ ignore = ENV["IGNORE"].split(File::PATH_SEPARATOR).flat_map { |path| expand(path
 
 # Setup ImageOptim options.
 image_optim = ImageOptim.new(
-  :advpng => {
-    :level => 3
-  },
-  :pngout => false,
+  :advpng => false, # redundant with oxipng
+  :pngcrush => false, # redundant with oxipng
+  :pngout => false, # redundant with oxipng
+  :optipng => false, # redundant with oxipng
   :svgo => {
     :enable_plugins => %w[
         cleanupAttrs cleanupListOfValues cleanupNumericValues convertColors convertStyleToAttrs
